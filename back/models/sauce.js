3253875172ff957ba416714ab7fb443a726fa0a2
reuby-dev <delphineb57@gmail.com>
2021-03-23 10:52:58 +0100

    création du schema de la sauce

---
+++ back/models/sauce.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema({
+    //todo : objectID: {type: orderID, required: true},
+    userId: {type: String, required: true},
+    name: {type: String, required: true},
+    manufacturer: {type: String, required: true},
+    description: {type: String, required: true},
+    mainPepper: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    heat: {type: Number, required: true},
+    likes: {type: Number, required: true},
+    dislikes: {type: Number, required: true},
+    userLiked: {[type: String, required: true]},
+    userDisliked: {[type: String, required: true]}
+});
+
+module.exports = mongoose.mondel('Sauce', sauceSchema);
