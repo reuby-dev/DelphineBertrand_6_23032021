a581cf7336f9c18efc964f84408d378eb8af0d44
reuby-dev <delphineb57@gmail.com>
2021-03-23 13:45:30 +0100

    creation du schema utilisateur

---
+++ back/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator'); //pré-valide les informations avant de les enregistrer
+
+const userSchema = mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+});
+
+userSchema.plugin(uniqueValidator); //aucun des deux utilisateurs ne peut partager la meme adresse mail
+
+module.exports = mongoose.model('User', userSchema);
--- back/routes/sauce.js
+++ back/routes/sauce.js
@@ -1,28 +1,6 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.post('/', (req, res, next) => {
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
 
-router.use('/', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      }
-    ];
-    res.status(200).json(stuff);
-});
 
 module.exports = router;
