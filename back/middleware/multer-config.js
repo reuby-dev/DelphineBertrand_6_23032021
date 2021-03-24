478f9b36017c4ae7a4c2ecf01de8266de8cc076b
reuby-dev <delphineb57@gmail.com>
2021-03-24 10:28:34 +0100

    création du middleware de gestion des fichiers

---
+++ back/middleware/multer-config.js
@@ -0,0 +1,20 @@
+const multer = require('multer');
+
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png' : 'png'
+};
+
+const storage = multer.diskStorage({ //où enregistrer les fichiers entrants
+    destination: (req, file, callback) => {
+        callback(null, 'images'); //enregistre dans le dossier images
+    },
+    filename: (req, file, callback) => { 
+        const name = file.originalname.split(' ').join('_'); //utilise le nom d'origine, remplace les espaces vides par des _
+        const extension = MIME_TYPES[file.mimetype]; //constante dictionnaire de type MIME pour résoudre l'extension de fichier appropriée
+        callback(null, name + Date.now() +'.'+ extension); //ajoute un timestamp comme nom de fichier
+    }
+});
+
+module.exports = multer({storage: storage}).single('image'); //exporte l'élément multer et passe la constante storage, en indiquant que nous gérons uniquement des images
