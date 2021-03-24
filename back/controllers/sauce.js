3b654578900e342b93dc845b984f6b8989b8b4da
reuby-dev <delphineb57@gmail.com>
2021-03-24 10:46:26 +0100

    fonction delete du back

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/sauce');
+const fs = require('fs'); //file system de node
 
 // exports.createThing = (req, res, next) => {
 //     const thingObject = JSON.parse(req.body.thing); //analyse pour le rendre utilisable sous forme de data, et non de json
@@ -21,4 +22,17 @@ const Sauce = require('../models/sauce');
 //     Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id }) //s'il existe, traite la nouvelle image
 //       .then(() => res.status(200).json({ message: 'Objet modifié !'}))
 //       .catch(error => res.status(400).json({ error }));
+// };
+
+// exports.deleteThing = (req, res, next) => { 
+//     Thing.findOne({ _id: req.params.id }) //utilise l'id recu comme paramètre pour accéder à la sauce correspondant dans la bdd
+//       .then(thing => {
+//         const filename = thing.imageUrl.split('/images/')[1]; //sépare le nom de fichier
+//         fs.unlink(`images/${filename}`, () => { //supprime le fichier
+//           Thing.deleteOne({ _id: req.params.id }) //le fichier à supprimer 
+//             .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+//             .catch(error => res.status(400).json({ error }));
+//         });
+//       })
+//       .catch(error => res.status(500).json({ error }));
 // };
