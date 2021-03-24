960fb641c02ce6633d01a27cd55a93d40034f609
reuby-dev <delphineb57@gmail.com>
2021-03-24 10:04:29 +0100

    création du token d'identification

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10) //appelle la fonction de hachage et "sale" le mdp 10 fois
@@ -15,22 +16,26 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+    User.findOne({ email: req.body.email }) //vérifie que l'email entré par l'utilisateur correspond à un utilisateur existant de la BDD
       .then(user => {
         if (!user) {
           return res.status(401).json({ error: 'Utilisateur non trouvé !' });
         }
-        bcrypt.compare(req.body.password, user.password)
+        bcrypt.compare(req.body.password, user.password) //compare le mdp entré par l'utilisateur avec le hash enregistré en BDD
           .then(valid => {
             if (!valid) {
               return res.status(401).json({ error: 'Mot de passe incorrect !' });
             }
             res.status(200).json({
               userId: user._id,
-              token: 'TOKEN'
+              token: jwt.sign( //encode un nouveau token
+                  {userId: user._id}, //le token contient l'id de l'utilisateur
+                  'RANDOM_TOKEN_SECRET',
+                  {expiresIn: '24h'}
+              )
             });
           })
           .catch(error => res.status(500).json({ error }));
       })
       .catch(error => res.status(500).json({ error }));
-  };
+};
