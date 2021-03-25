f09030e5c8542a14beeaca93fb9d44417d2bafca
reuby-dev <delphineb57@gmail.com>
2021-03-25 11:11:31 +0100

    mise à jour des routes en attendant le mentorat de ce soir

--- back/models/sauce.js
+++ back/models/sauce.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
-    // objectID: {type: String, required: true},
+    objectID: {type: String, required: true},
     // userId: {type: String, required: true},
     name: {type: String, required: true},
     manufacturer: {type: String, required: true},
@@ -9,10 +9,10 @@ const sauceSchema = mongoose.Schema({
     mainPepper: {type: String, required: true},
     // imageUrl: {type: String, required: true},
     heat: {type: Number, required: true},
-    // likes: {type: Number, required: true},
-    // dislikes: {type: Number, required: true},
-    // userLiked: {[]},
-    // userDisliked: {[]},
+    likes: {type: Number, required: true},
+    dislikes: {type: Number, required: true},
+    usersLiked: {type: Array, required: true},
+    usersDisliked: {type: Array, required: true},
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
--- back/routes/sauce.js
+++ back/routes/sauce.js
@@ -10,7 +10,7 @@ const sauceCtrl = require('../controllers/sauce');
 router.get('/', sauceCtrl.getAllSauces);
 router.post('/', multer, sauceCtrl.createSauce);
 router.get('/:id', sauceCtrl.getOneSauce);
-// router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-// router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.put('/:id',  multer, sauceCtrl.modifySauce);
+router.delete('/:id', sauceCtrl.deleteSauce);
 
 module.exports = router;
